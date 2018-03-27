50b5d75110407b8c93268011ba1c8295905f0786
Unknown <philguyonnet@gmail.com>
2018-03-27 14:35:29 +0200

    factory

--- src/fr/eni/javaee/projetQCM/dal/LoginDAO.java
+++ src/fr/eni/javaee/projetQCM/dal/LoginDAO.java
@@ -2,4 +2,7 @@ package fr.eni.javaee.projetQCM.dal;
 
 public interface LoginDAO {
 
+	public void User();
+		
+	
 }
--- src/fr/eni/javaee/projetQCM/dal/LoginDAOJdbcImpl.java
+++ src/fr/eni/javaee/projetQCM/dal/LoginDAOJdbcImpl.java
@@ -1,5 +1,56 @@
 package fr.eni.javaee.projetQCM.dal;
 
-public class LoginDAOJdbcImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import fr.eni.javaee.projetQCM.bo.User;
 
+public class LoginDAOJdbcImpl implements LoginDAO {
+
+	
+	public static final String SELECT="SELECT * FROM UTILISATEUR(id, nom, prenom, email, password, codeProfil);";
+	
+	
+		public void User(int id, String nom, String prenom, String email, String password, int codeProfil, String codePromo) {
+			Connection conn = null;
+			User user;
+			PreparedStatement rqt = null;
+			ResultSet rs;
+
+			try { 
+				conn = ConnectionProvider.getConnection();
+				rqt = conn.prepareStatement(SELECT);
+				rqt.setInt(1, id);
+				rqt.setString(2, nom);
+				rqt.setString(3, prenom);
+				rqt.setString(4, email);
+				rqt.setString(5, password);
+				rqt.setInt(6, codeProfil);
+				rqt.setString(7, codePromo);
+				
+				rs = rqt.executeQuery();
+
+				if (rs.next()) {
+
+					user = new User(rs.getInt("id"),
+							rs.getString("Nom"), rs.getString("Prenom"),
+							rs.getString("email"),rs.getString("password"),
+							rs.getInt("codeProfil"), rs.getString("codePromo"));
+					
+				}
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+			return user;
+		}
+
+
+		@Override
+		public void User() {
+			// TODO Auto-generated method stub
+			
+		}
+	
+	
+	
 }
