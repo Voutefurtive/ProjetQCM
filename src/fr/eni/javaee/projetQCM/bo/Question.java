49d50484682d0c861c21cbd605cf57c16a788f2c
Vincent Ht <hersant.vincent@gmail.com>
2018-03-27 14:21:45 +0200

    Gestion Questions : bo + dal

---
+++ src/fr/eni/javaee/projetQCM/bo/Question.java
@@ -0,0 +1,68 @@
+package fr.eni.javaee.projetQCM.bo;
+
+import java.util.List;
+
+public class Question {
+	
+	private int id;
+	private String enonce;
+	private String media;
+	private int points;
+	private List<Proposition> propositions;
+	private boolean estMarquee;
+	
+	public Question() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEnonce() {
+		return enonce;
+	}
+
+	public void setEnonce(String enonce) {
+		this.enonce = enonce;
+	}
+
+	public String getMedia() {
+		return media;
+	}
+
+	public void setMedia(String media) {
+		this.media = media;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public List<Proposition> getPropositions() {
+		return propositions;
+	}
+
+	public void setPropositions(List<Proposition> propositions) {
+		this.propositions = propositions;
+	}
+
+	public boolean isEstMarquee() {
+		return estMarquee;
+	}
+
+	public void setEstMarquee(boolean estMarquee) {
+		this.estMarquee = estMarquee;
+	}
+	
+	
+	
+}
